de16a73aae6ccf5bb37bfc96ac106f14daf0668c
Reyzartz <reyan@revenuehero.io>
2025-01-11 03:23:09 +0530

    refactor: removed vite-plugin-static-copy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
